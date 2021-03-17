bd95b4a01c74e378b24dc5b27489bec1da73f7b0
roccia94 <roccia94@hotmail.it>
2021-03-17 18:04:50 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
-import "./style.css";
-import Clock from  "./Clock.js"
+import React, { Component } from 'react';
+import Clock from  './Clock'
+ 
+class App extends Component {
 
-export default function App() {
-  return (
-    <React.Fragment>
-    <div>
-      <h1>Ora e data attraverso un componente</h1>
-    </div>
-    <Clock country="Italia" timezone="0"/>
-    <Clock country="qualche posto" timezone="10"/>
-    </React.Fragment>
-  );
+  render() {
+    return (
+      <React.Fragment>
+      <div className="App">
+       <h1> Data e ora</h1>
+      </div>
+      <Clock secs="1" country="Italy" timezone="0" />
+      <Clock  secs="5" country="qualche posto"  timezone="2" />
+      </React.Fragment>
+    );
+  }
 }
+
+export default App;
--- src/Clock.js
+++ src/Clock.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
-function clock(props){
-  const tempo = Date.now() + props.timezone * 3600 * 1000;
-  const data = new Date(tempo);
-  return   <h2>In {props.country} è il giorno { data.toLocaleDateString() + ' ' + data.toLocaleTimeString()}</h2>
+
+class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+       this.state = {
+            date: new Date(),
+            timezone : 1
+        };
+        
+    }
+
+   render(){
+    const tempo =  this.state.date.getTime()  + this.props.timezone  * 3600 * 1000 ;
+    const data = new Date(tempo);
+
+    return <h2> In { this.props.country }  is   {data.toLocaleTimeString() }</h2>
+   }
+
+   tick = () => {
+
+   this.setState({
+       date: new Date()
+   }); 
+};
+
+   componentDidMount(){
+       console.log('Component did update')
+      this.interval =  setInterval(this.tick, this.props.secs*1000);
+   }
+   componentWillUnmount(){
+       clearInterval(this.interval);
+   }
 }
 
-export default clock;
+export default Clock;
